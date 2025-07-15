c79619d4562d3a22d87af061d93499e7858cfa7e
DiegoCaladoProgramador <diegocaladoprogramador@gmail.com>
2025-07-15 18:25:18 -0300

    configurar o prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Todos nós vamos conseguir virar GRANDES DESENVOLVEDORES</h1>
+function Home() {
+  return <h1>Todos nós vamos conseguir virar GRANDES DESENVOLVEDORES</h1>;
 }
 
-export default Home
+export default Home;
